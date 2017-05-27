caa6b05c920d8481e95e5a38d12895de0d366605
Raymund Lew <raymund.lew@gmail.com>
2017-05-27 00:31:36 -0700

    Put gd (git diff) purely into a fish function

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -3,5 +3,8 @@ alias gs 'git status -sb'
 eval (hub alias -s)
 alias vi nvim
 alias vim nvim
-alias gd 'git dsf'
 alias gb 'git branch -a'
+
+function gd
+  git diff --color $argv | diff-so-fancy | less --tabs=4 -RFX
+end
