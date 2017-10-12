8533ed373f2d166c02eb2dcd76005a9ad173199f
Raymund Lew <raymund.lew@gmail.com>
2017-10-12 14:09:05 -0700

    Add virtualfish plugin, and conda integration

--- fish/config.fish
+++ fish/config.fish
@@ -8,11 +8,12 @@ set tacklebox_theme entropy
 # Which modules would you like to load? (modules can be found in ~/.tackle/modules/*)
 # Custom modules may be added to ~/.tacklebox/modules/
 # Example format: set tacklebox_modules virtualfish virtualhooks
+set tacklebox_modules virtualfish virtualhooks
 
 # Which plugins would you like to enable? (plugins can be found in ~/.tackle/plugins/*)
 # Custom plugins may be added to ~/.tacklebox/plugins/
 # Example format: set tacklebox_plugins python extract
-set tacklebox_plugins extract up docker
+set tacklebox_plugins extract up docker pip python
 
 # Load Tacklebox configuration
 . ~/.tacklebox/tacklebox.fish
@@ -31,14 +32,14 @@ set -x PACKAGE_DIRS $HOME/workspace/meteor/packages
 
 set -x GOPATH $HOME/workspace/golang
 
-set -x CONDAPATH $HOME/miniconda3
+set -x CONDA_PATH /usr/local/miniconda3/bin
 
-set PATH /usr/local/sbin ~/.composer/vendor ~/.pub-cache/bin ~/.phpenv/bin $GOPATH/bin $CONDAPATH/bin $PATH
+set PATH /usr/local/sbin $GOPATH $GOPATH/bin $CONDA_PATH $PATH
 
 source /usr/local/share/chruby/chruby.fish
 
 source /usr/local/share/chruby/auto.fish
 
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+source /usr/local/miniconda3/etc/fish/conf.d/conda.fish
 
-source /Users/raymundlew/.phpbrew/phpbrew.fish
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
